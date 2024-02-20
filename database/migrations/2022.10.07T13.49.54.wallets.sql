0d85d4ef8d0726290364904637b6b95e1b8e50ac
Sasha <azavialova2005@gmail.com>
2024-02-20 10:17:41 +0300

    c1.3

--- database/migrations/2022.10.07T13.49.54.wallets.sql
+++ database/migrations/2022.10.07T13.49.54.wallets.sql
@@ -4,6 +4,7 @@ CREATE TABLE "wallets" (
   "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
   "balance" integer NOT NULL DEFAULT '0',
   "userId" character varying NOT NULL,
+  "someField" integer NOT NULL,
   CONSTRAINT "UQ_35472b1fe48b6330cd349709564" UNIQUE ("userId"),
   CONSTRAINT "PK_bec464dd8d54c39c54fd32e2334" PRIMARY KEY ("id")
 )
